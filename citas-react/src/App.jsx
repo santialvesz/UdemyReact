b56900769fe2dd6634ef1dcca924f1d79e2eac6c
Santiago Alves <5102@holbertonstudents.com>
2023-02-07 18:59:04 -0300

    first commit

---
+++ citas-react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Formulario from './components/Formulario'
+import ListadoPacientes from './components/ListadoPacientes'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Formulario/>
+      <ListadoPacientes/>
+    </div>
+  )
+}
+
+export default App
---
+++ citas-react/src/components/ListadoPacientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListadoPacientes = () => {
+  return (
+    <div>
+        <h2>ListadoPacientes</h2>
+    </div>
+  )
+}
+
+export default ListadoPacientes
